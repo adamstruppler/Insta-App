75673a6676116b660736214d609becaf1aadf6ec
adamstruppler <adamstruppler@gmail.com>
2017-12-05 19:09:37 -0700

    reverted code base

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,12 @@
-import React, {Component} from 'react'
-import Header from './Header'
-import Form from './Form'
-import PostList from './PostList'
-import $ from 'jquery'
+import React from 'react'
 
-class App extends Component {
-  state = {
-    posts: undefined,
-    title: undefined,
-    img: undefined,
-    caption: undefined
-  }
-
-componentDidMount() {
-  this.loadPostsFromServer()
-}
-
-loadPostsFromServer = () => {
-  $.ajax({
-    url: '/api/post',
-    method: 'GET'
-  }).done((response) => {
-    this.setState({posts: response.posts})
-  })
-}
-
-
-submitPostsToServer = () => {
-  const newPost = {
-    title: this.state.title,
-    img: this.state.img,
-    caption: this.state.caption
-  }
-  $.ajax({
-    url: '/api/post',
-    method: 'POST',
-    data: newPost
-  }).done((response) => {
-    this.loadPostsFromServer()
-  })
-}
-
-updateTitle = (e) => this.setState({title: e.target.value})
-updateImg = (e) => this.setState({img: e.target.value})
-updateCaption = (e) => this.setState({caption: e.target.value})
-
-render() {
+const App = () => {
   return (
     <div>
-       <Header />
-       <Form />
-    {
-      this.state.posts
-      ? <PostList posts={this.state.post} />
-      :'No Post'
-    }
+      <h1>Hello World :)</h1>
     </div>
-   
   )
 }
 
-}
 
 export default App
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const Post = require('./post/Post')
 const app = express()
 
 const port = 3001
@@ -12,30 +11,6 @@ mongoose.connect('mongodb://localhost/insta-app_DB')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('./config/error-handler'))
-
-const post = {title: 'Post', img: 'https://dog.jpg', caption: 'picture caption'}
-
-app.post('/api.post', (req, res) => {
-  const {title, img, caption} = req.body
-  const newPost = {title, img, caption}
-  Post(newPost).save((err, savedPost) => {
-    if(err) {
-      res.json({error: err})
-    } else {
-      res.json({msg: 'Uploaded', data: savedPost})
-    }
-  })
-})
-
-app.get('/api.post', (req,res) => {
-  Post.find((err, posts) => {
-    if(err) {
-      res.json({error: err})
-    } else {
-      res.json({msg: 'Uploaded', posts})
-    }
-  })
-})
 
 const server = app.listen(port, () => console.log(`:( Running on port: ${port}`))
 
